70dd3e66e2460480ab2dbbea70ff33f8d3fc7858
Moritz Christ <moritz-christ@t-online.de>
2023-10-10 09:20:21 +0200

    Initital commit

---
+++ src/main/java/de/moritz/cs2gsi/Cs2GsiApplication.java
@@ -0,0 +1,13 @@
+package de.moritz.cs2gsi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cs2GsiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Cs2GsiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/moritz/cs2gsi/Cs2GsiApplicationTests.java
@@ -0,0 +1,13 @@
+package de.moritz.cs2gsi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cs2GsiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
